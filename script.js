8a78f251763d6298f22ab964ccc9dbeabe9382ac
reetah589 <reetah589@gmail.com>
2021-11-07 19:19:03 -0600

    added code to createtask handler

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskNameInput = document.querySelector("select[name='task-type']").value;
 
 var createTaskHandler = function(event) {
     event.preventDefault();
-    var taskNameInput = document.querySelector("select[name='task-type']").value;
     
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement("div");
-    listItemEl.textContent = taskNameInput;
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+    //listItemEl.textContent = taskNameInput;
     tasksToDoEl.appendChild(listItemEl);
 };
 
