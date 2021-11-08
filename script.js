56ee59d907059f1d487936f05c3c10510eae1dfb
reetah589 <reetah589@gmail.com>
2021-11-07 19:35:41 -0600

    js file code cleanup

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var taskNameInput = document.querySelector("select[name='task-type']").value;
+
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
     
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
     listItemEl.appendChild(taskInfoEl);
-    //listItemEl.textContent = taskNameInput;
+    
     tasksToDoEl.appendChild(listItemEl);
 };
 
